d4d59fd96fd13777f2f59e25def6f2255da10ecf
Cristian Arraya <arrayacristian75@gmail.com>
2024-01-27 18:37:36 -0300

    El desafio se muestra en la pantalla y con animacion en cada letra

--- 1D4GameJam/Assets/Scripts/UIController.cs
+++ 1D4GameJam/Assets/Scripts/UIController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     [SerializeField] GameObject winnerPanel;
+    [SerializeField] Text challenge;
 
-    public void ActivateWinnerPanel()
+    public void ActivateWinnerPanel(string challenge)
     {
         winnerPanel.SetActive(true);
     }
